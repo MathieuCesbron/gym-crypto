3c32c41680436cee5ea1047945a35406cae38823
mathieu cesbron <mathieu.cesbron@ecam-rennes.com>
2019-11-12 18:47:25 +0100

    data creation added

---
+++ keys.py
@@ -0,0 +1,2 @@
+apiKey = "DKpYeQJDdxgo2mSXIqKnk70QmOy8urDQjJFjPDk06H4waWrj1irLnuFFfHpXMCcC"
+secretKey = "PIp3AKATJfillNs9Lsm1pbUaTMQqMTXFfGS5H77DMwzi6a1GPLOyt78hv03kcf1f"
--- test.py
+++ test.py
@@ -1,7 +1,4 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('data/BTCUSDT.csv')
-
-frame = np.array([df.loc[0:5, 'open'].values, df.loc[0:5, 'open'].values])
-print(frame)
+#:df = pd.read_csv('data/BTCUSDT.csv')
