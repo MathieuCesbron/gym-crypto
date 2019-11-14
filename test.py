0b282875a41ec8e1ef3cce70c5af9fef78c03376
mathieu cesbron <mathieu.cesbron@ecam-rennes.com>
2019-11-14 21:04:15 +0100

    preparation for the dockerfile

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-import pandas as pd
-import numpy as np
-
-#:df = pd.read_csv('data/BTCUSDT.csv')
+# Standardize the data attributes for the Iris dataset.
+from sklearn.datasets import load_iris
+from sklearn import preprocessing
+# load the Iris dataset
+iris = load_iris()
+print(iris.data.shape)
+# separate the data and target attributes
+X = iris.data
+y = iris.target
+# standardize the data attributes
+standardized_X = preprocessing.scale(X)
+print(standardized_X)
