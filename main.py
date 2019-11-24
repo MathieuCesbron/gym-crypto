ef2467cae993ade2bfac2a404bdaeb456c061c4f
MathieuCesbron <mathieu.cesbron@ecam-rennes.com>
2019-11-24 14:46:20 +0100

    zip file ignored

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ df = pd.read_csv('data/BTCUSDT.csv')
 env = DummyVecEnv([lambda: CryptoEnv(df)])
 
 # Instanciate the agent
-model = PPO2(MlpPolicy, env, gamma=1, learning_rate=0.01, verbose=1)
+model = PPO2(MlpPolicy, env, gamma=1, learning_rate=0.0001, verbose=0)
 
 # Train the agent
 total_timesteps = int(os.getenv('TOTAL_TIMESTEPS', 1000000))
@@ -26,7 +26,7 @@ env.render(graph_reward=True)
 # Trained agent performence
 obs = env.reset()
 env.render()
-for i in range(1100):
+for i in range(100000):
     action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
     env.render(print_step=True)
