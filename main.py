029df774a5939150e2b67e6108dfdb6327d66aae
MathieuCesbron <mathieu.cesbron@ecam-rennes.com>
2019-11-26 15:24:43 +0100

    useless column deleted

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from env import CryptoEnv
 import pandas as pd
 import os
 
-df = pd.read_csv('data/BTCUSDT.csv')
+df = pd.read_csv('data/BTCUSDT.csv', index_col=0)
 
 env = DummyVecEnv([lambda: CryptoEnv(df)])
 
